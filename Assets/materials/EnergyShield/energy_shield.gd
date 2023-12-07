ba9a0dfae183fdbb52330f82a78a8bd0137bb0f0
nan0m <74869542+nan0m@users.noreply.github.com>
2023-12-07 13:33:09 +0100

    shader update and fixes

--- Assets/materials/EnergyShield/energy_shield.gd
+++ Assets/materials/EnergyShield/energy_shield.gd
@@ -15,7 +15,7 @@ class_name EnergyShield
 const _max_16bit_uint: = 65535
 
 class Impact:
-	var location := Vector3.ZERO
+	var location := Vector2.ZERO
 	var total_lifetime: float = 1.0
 	var elapsed_lifetime:float = 0.0
 	#	var color := Color.NAVAJO_WHITE
@@ -25,9 +25,9 @@ var _current_impacts: Array[Impact] = []
 @export var pause_shield: bool = false
 
 ## Call this method to add an impact effect at that global position
-func add_impact(global_pos: Vector3, total_lifetime: float):
+func add_impact(global_pos: Vector2, total_lifetime: float):
 	var im := Impact.new()
-	im.location = global_pos
+	im.location = (global_pos - self.global_position).normalized()
 	im.total_lifetime = total_lifetime
 	_current_impacts.append(im)
 
@@ -39,10 +39,11 @@ func add_impact(global_pos: Vector3, total_lifetime: float):
 func _ready():
 	pass
 	#you can add impacts here for testing.
-#	add_impact(Vector3(10,10,0.0),100.0)
-#	add_impact(Vector3(100,100,0.0),10.0)
-#	add_impact(Vector3(800,800,0.0),20.0)
-#	add_impact(Vector3(600,1230,0.0),20.0)
+#	add_impact(Vector2(10,10),100.0)
+#	add_impact(Vector2(100,100),10.0)
+	add_impact(Vector2(0,-2000),15.0)
+#	add_impact(Vector2(800,0),40.0)
+#	add_impact(Vector2(600,1230),20.0)
 
 
 func _process(delta):
@@ -65,7 +66,9 @@ func _update_texture(delta: float):
 
 		for i in _current_impacts.size():
 			var impact: Impact = _current_impacts[i]
-			var pos_elapsed =  _pack_position_to_color(impact.location)
+#			var normalized_direction: Vector2 = (impact.location - self.global_position).normalized()
+#			var pos_elapsed =  _pack_position_to_color(impact.location)
+			var pos_elapsed =  _pack_direction_vec_to_color(impact.location)
 			pos_elapsed.b = impact.elapsed_lifetime / impact.total_lifetime
 			tmp_img.set_pixel(0, i, pos_elapsed)
 			
@@ -74,8 +77,15 @@ func _update_texture(delta: float):
 		self.material.set_shader_parameter("_impact_data_texture",impact_tex)
 
 ## transform position value into 0 -> 1 range for storing in texture.
-func _pack_position_to_color(global_pos: Vector3) -> Color:
+func _pack_position_to_color(global_pos: Vector2) -> Color:
 	var col := Color()
 	col.r = (global_pos.x / _max_16bit_uint) + 0.5
 	col.g = (global_pos.y  / _max_16bit_uint) + 0.5
 	return col
+
+func _pack_direction_vec_to_color(direction: Vector2) -> Color:
+	var col := Color()
+	col.r = (direction.x / 2.0) + 0.5
+	col.g = (direction.y  / 2.0) + 0.5
+	return col
+
